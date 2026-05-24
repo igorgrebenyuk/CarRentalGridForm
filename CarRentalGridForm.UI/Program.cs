5376c93f64bf110460b9bc09cf1e69b2d2fa1d80
Igor Grebenyuk <igorgrebenyuk2008@gmail.com>
2026-05-24 13:49:59 +0300

    Сделал логирование производительности и сбор логов , добавил нужные нагет пакеты и создал необходимый класс в БЛ проекта
    * Создал класс для логирования БЛ
    
    * Написал  логеры для БЛ
    
    * Настроил seq , дописал програм и добавил реализацию ICarService в класс враппера
    
    * Чуть-чуть поправил
    
    * поправил замечания ревьювера

--- CarRentalGridForm.UI/Program.cs
+++ CarRentalGridForm.UI/Program.cs
@@ -2,28 +2,44 @@ using CarRentalGridForm.BL;
 using CarRentalGridForm.BL.Contracts;
 using CarRentalGridForm.DAL.Contracts;
 using CarRentalGridForm.UI;
-using CarRentalGridForm.DAL; 
-
+using CarRentalGridForm.DAL;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 
 namespace CarRentalGridForm
 {
-    /// <summary>
-    /// Главный класс приложения, содержащий точку входа.
-    /// </summary>
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа в приложение.
-        /// </summary>
         [STAThread]
         private static void Main()
         {
+            var seqApiKey = "mqoY4hJHcRmN6lLAXLgL";
+
+            var serilogLogger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Debug()
+                .WriteTo.Seq(
+                    serverUrl: "http://localhost:5341",
+                    apiKey: seqApiKey,
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Debug
+                )
+                .WriteTo.File("logs/car-perf-.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            var loggerFactory = new SerilogLoggerFactory(serilogLogger);
+            var logger = loggerFactory.CreateLogger<CarServiceLogWrapper>();
+
             ApplicationConfiguration.Initialize();
 
             ICarRepository repository = new CarRepository();
             ICarService service = new CarService(repository);
 
-            Application.Run(new MainForm(service));
+            ICarService loggingWrapper = new CarServiceLogWrapper(service, logger);
+
+            Application.Run(new MainForm(loggingWrapper));
+
+            Log.CloseAndFlush();
         }
     }
 }
