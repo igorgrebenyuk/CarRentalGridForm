8b42a9630c5a4eac26429c797c265d61abce0c5f
Игорь <igorgrebenyuk2008@gmail.com>
2026-05-25 19:30:53 +0300

    Сделал так чтобы БД работала синхронно

--- CarRentalGrid.Storage.EFStorage/CarRentalContext.cs
+++ CarRentalGrid.Storage.EFStorage/CarRentalContext.cs
@@ -9,4 +9,8 @@ public class CarRentalContext : DbContext
 
     public CarRentalContext() => Database.EnsureCreated();
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+    }
 }
